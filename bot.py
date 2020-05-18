8b57a75d486856d30a64100f66948b0c7f732158
dark0ghost <diamondlego018@gmail.com>
2020-05-18 13:24:17 +0200

    setup

--- bot.py
+++ bot.py
@@ -8,7 +8,8 @@ from server.server import hello,faviconico
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     app = web.Application()
-    app.add_routes([web.get('/', hello)])
+    app.add_routes([web.post('/', hello)])
+    app.add_routes([web.post('/', hello)])
     app.add_routes([web.get('/favicon.ico', faviconico)])
     Bot = mod.core.BotStart(loop)
     loop.run_until_complete(Bot.on_startup())
