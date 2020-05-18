a02da6042f0407f9217f2f3bf32d18009c7789fc
dark0ghost <diamondlego018@gmail.com>
2020-05-18 14:34:56 +0200

    setup

--- bot.py
+++ bot.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
         web.get('/favicon.ico', faviconico),
         web.post('/favicon.ico', faviconico)
     ])
-    #app.on_startup.append(mode.core.on_startup)
-    #app.add_routes([web.post(mode.core.config.webhook["path"], mode.core.execute)])
+    app.on_startup.append(mode.core.on_startup)
+    app.add_routes([web.post(mode.core.config.webhook["path"], mode.core.execute)])
     web.run_app(app, port=mode.core.config.server["port"], host=mode.core.config.server["host"])
