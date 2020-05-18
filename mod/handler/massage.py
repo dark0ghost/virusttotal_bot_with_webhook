672628dfc89eefd0f0eb0cc054bb9d6e767d6509
dark0ghost <diamondlego018@gmail.com>
2020-05-18 11:42:00 +0200

    setup

--- mod/handler/massage.py
+++ mod/handler/massage.py
@@ -1,6 +1,13 @@
-from aiogram import types
+from aiogram import types, Dispatcher, Bot
 from aiogram.dispatcher.webhook import SendMessage
+
+from lib.virus_total import Virustotal
+from mod import text
 
 
 async def echo(message: types.Message):
-    return SendMessage()
+    return SendMessage(text.TextResponse.START)
+
+
+
+def rigester_handler(db: Dispatcher, bot: Bot,virusttotal: Virustotal):
