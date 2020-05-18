a2379e36331ba24c39c0a8c60e4fc56a98d21eab
dark0ghost <diamondlego018@gmail.com>
2020-05-18 17:39:36 +0200

    chcek

--- module/core.py
+++ module/core.py
@@ -56,6 +56,10 @@ async def check_file(message: types.Message):
             print(response["md5"])
             response_report = await virustotal.file_report(resource=response['scan_id'])
             await asyncio.sleep(5000)
+            while response_report["response_code"] == -2:
+                await asyncio.sleep(5000)
+                response_report = await virustotal.file_report(resource=response['scan_id'])
+
             await message.answer(f"""scan `id{response['scan_id']}`
                                   search vulnerabilities  {response_report['positives']}  is {response_report['positives']}""",
                                  parse_mode=types.ParseMode.MARKDOWN,
