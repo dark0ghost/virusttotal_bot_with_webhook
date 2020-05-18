54103c24da9aa1401fe5196799755cb6708f2c0f
dark0ghost <diamondlego018@gmail.com>
2020-05-18 20:51:42 +0200

    chcek

--- module/text.py
+++ module/text.py
@@ -3,4 +3,5 @@ class TextResponse:
     def __init__(self):
         self.START = """Hello, this bot  checks for vulnerabilities in a file using virustotal.com
          check https://virustotalbotaio.herokuapp.com/
-         how use: send document and wait response"""
+         how use: send document and wait response
+         github - https://github.com/dark0ghost/virusttotal_bot_with_webhook """
